191a728fafd33b7d886cbf8e4eb147a6c0362263
CDjepeno <djepeno.christopher@gmail.com>
2020-12-17 10:34:31 +0100

    Create fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -26,51 +26,56 @@ class AppFixtures extends Fixture
             $manager->persist($editor);
 
             $editors[] = $editor; 
-        }
-    
-        // Ont gère les genres
-        $genres = ['Litterature','Terreur','Science','BD','Policier'];
-        foreach($genres as $g){
-            $genre = new Genre;
-            $genre->setTitle($g);
-
-            $manager->persist($genre);
-
-            // Ont gère les livres
-            for($b=0; $b<=mt_rand(3,5); $b++){
-                $book = new Book;
-                $book->setTitle('book'.$b)
-                     ->setPrice(mt_rand(3, 7))
-                     ->setIsbn(45)
-                     ->setYear(mt_rand(1990,1995))
-                     ->setYear()
-
-            }
-        }
-
-        
+        }   
 
         // Ont gère les nationalités
-        $nationalities=['Russe','Espagnol','Américain','Français','Irlandais','Britanique','Allemand','Suedois','Italien','Japonais' ];
+        $nationalities=['Russe','Espagnol','Américain','Français','Irlandais','Britanique','Allemand','Suedois','Italien','Japonais'];
         foreach($nationalities as $nat) {
             $nationality = new Nationality;
 
             $nationality->setTitle($nat);
             $manager->persist($nationality);
             
-            
-
-            // Ont gère les auteurs
-            for($a=0; $a<=mt_rand(2.3); $a++){
+             // Ont gère les auteurs
+             $authors = [];
+             for($a=0; $a<=mt_rand(2,3); $a++){
                 $author = new Author;
                 $author->setFirstname($faker->firstname())
-                ->setLastname($faker->lastname())
-                ->setNationality($nationality)
-                ->
+                        ->setLastname($faker->lastname())
+                        ->setNationality($nationality);
+                        
                 $manager->persist($author);
+                $authors[]= $author;
             }
+            dd(count($authors));
 
+           
         }
+         // Ont gère les genres
+         $genres = ['Litterature','Terreur','Science','BD','Policier'];
+         foreach($genres as $g){
+             $genre = new Genre;
+             $genre->setTitle($g);
+
+             $manager->persist($genre);
+
+             // Ont gère les livres
+             for($b=0; $b<=mt_rand(3,5); $b++){
+                 $book = new Book;
+                 $editor = $editors[mt_rand(0,count($editors) -1)];
+                 $author = $authors[mt_rand(0,count($authors) -1)];
+                 $book->setTitle('book'.$b)
+                     ->setPrice(mt_rand(3, 7))
+                     ->setIsbn(45)
+                     ->setYear(mt_rand(1990,1995))
+                     ->setLangue("italia")
+                     ->setAuthor($author)
+                     ->setEditor($editor)
+                     ->setGenre($genre);
+
+                $manager->persist($book);
+             }
+         }
 
 
         $manager->flush();
