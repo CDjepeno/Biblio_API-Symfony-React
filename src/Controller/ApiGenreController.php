6f3a36cdd1c8cadf769a9268d2bea78a9e8561af
CDjepeno <djepeno.christopher@gmail.com>
2020-12-17 19:15:30 +0100

    Create API for add genre

--- src/Controller/ApiGenreController.php
+++ src/Controller/ApiGenreController.php
@@ -4,12 +4,15 @@ namespace App\Controller;
 
 use App\Entity\Genre;
 use App\Repository\GenreRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ApiGenreController extends AbstractController
 {
@@ -42,5 +45,49 @@ class ApiGenreController extends AbstractController
             ]
         );
         return new JsonResponse($result, Response::HTTP_OK, [], true);
+    }
+
+    /**
+     * @Route("/api/genres", name="api_genre_create", methods={"POST"})
+     */
+    public function create(Request $request, SerializerInterface $serializer, EntityManagerInterface $em): Response
+    {
+        $data  = $request->getContent();
+        $genre = $serializer->deserialize(
+            $data,
+            Genre::class,
+            'json'      
+        );
+        
+        $em->persist($genre);
+        $em->flush();
+
+        return new JsonResponse(
+            "Le genre as bien été ajouter", 
+            Response::HTTP_CREATED,
+            ['location' => $this->generateUrl("api_genre_show", ['id' => $genre->getId(), UrlGeneratorInterface::ABSOLUTE_URL])],
+            true);
+    }
+
+    /**
+     * @Route("/api/genres", name="api_genre_update", methods={"PUT"})
+     */
+    public function update(Request $request, SerializerInterface $serializer, EntityManagerInterface $em): Response
+    {
+        $data  = $request->getContent();
+        $genre = $serializer->deserialize(
+            $data,
+            Genre::class,
+            'json'      
+        );
+        
+        $em->persist($genre);
+        $em->flush();
+
+        return new JsonResponse(
+            "Le genre as bien été ajouter", 
+            Response::HTTP_CREATED,
+            ['location' => $this->generateUrl("api_genre_show", ['id' => $genre->getId(), UrlGeneratorInterface::ABSOLUTE_URL])],
+            true);
     }
 }
